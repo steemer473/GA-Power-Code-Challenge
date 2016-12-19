399a576c4d6ff22e3cf00f89c2d161ddd7297885
steemer473 <stephanie.ragsdale.2013@gmail.com>
2016-12-19 02:49:59 -0500

    2nd commit - customized site

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -6,12 +6,3 @@
  * @version 1.0.5
  * Copyright 2016. MIT licensed.
  */
-(function ($, window, document, undefined) {
-
-  'use strict';
-
-  $(function () {
-    // FastShell
-  });
-
-})(jQuery, window, document);
