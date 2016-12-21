4530248e6bb358641af26578e905decab7a02538
steemer473 <stephanie.ragsdale.2013@gmail.com>
2016-12-21 12:52:04 -0500

    3nd commit - added additional assets for twitter page

--- app/common/twitter-header.php
+++ app/common/twitter-header.php
@@ -1,6 +1,6 @@
 <div id="twitter-header">
     <div class="container">
-        <h1 class="text-center">Twitter Holiday Cheer</h1>
-        <h3 class="text-center">Our Latest News & Tweets</h3>
+        <h1 class="text-center headline-text">Twitter Holiday Cheer</h1>
+        <h3 class="text-center headline-text">Our Latest News & Tweets</h3>
     </div>
 </div>
