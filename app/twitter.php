379268f3563ef90c4fb98de43575aebbf8c8fa56
steemer473 <stephanie.ragsdale.2013@gmail.com>
2016-12-21 13:50:37 -0500

    3nd commit - more twitter updates

--- app/twitter.php
+++ app/twitter.php
@@ -7,12 +7,31 @@
     <?php include 'common/menu.php';?>
     <?php include 'common/twitter-header.php';?>
     <div id="main" role="main">
-        <div class="container">
+        <div id="feeds-container" class="container">
             <!--Add Content Here...--->
-            <div id="timeline" class="col-md-9">
-                
+            <div id="timeline" class="col-md-7">
+                <a class="twitter-grid" href="https://twitter.com/TwitterDev/timelines/539487832448843776">National Park Tweets</a>
+                <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
-            <div id="tweets" class="col-md-9"></div>
+            <div id="tweets" class="col-md-5">
+                <div class="col-xs-12">
+                    <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">This middle school student is geared up for FIRST Tech Challenge - Empowering Michigan <a href="https://t.co/frZ11nSmwT">https://t.co/frZ11nSmwT</a></p>&mdash; DTE Energy (@DTE_Energy) <a href="https://twitter.com/DTE_Energy/status/811632340829093889">December 21, 2016</a></blockquote>
+                    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </div>
+                <div class="col-xs-12">
+                    <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Happy Holidays from the WPS Community Relations Group. <a href="https://t.co/HELHttniHp">pic.twitter.com/HELHttniHp</a></p>&mdash; WPS (@WIPublicService) <a href="https://twitter.com/WIPublicService/status/811638373744078848">December 21, 2016</a></blockquote>
+                    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </div>
+                <div class="col-xs-12">
+                    <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Check out this <a href="https://twitter.com/hashtag/infographic?src=hash">#infographic</a> to better understand <a href="https://twitter.com/hashtag/holiday?src=hash">#holiday</a> <a href="https://twitter.com/hashtag/shopping?src=hash">#shopping</a> trends: <a href="https://t.co/pEwzOKHGUG">https://t.co/pEwzOKHGUG</a></p>&mdash; Dalton Utilities (@DaltonUtilities) <a href="https://twitter.com/DaltonUtilities/status/811617251874996224">December 21, 2016</a></blockquote>
+                    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </div>
+                <div class="col-xs-12">
+                    <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Near record setting performance launches robotics team into state competition  <a href="https://t.co/NO4ESLq1oZ">https://t.co/NO4ESLq1oZ</a></p>&mdash; DTE Energy (@DTE_Energy) <a href="https://twitter.com/DTE_Energy/status/811602146227605506">December 21, 2016</a></blockquote>
+                    <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </div>
+
+            </div>
         </div>
     </div>
     <?php include 'common/footer.php';?>
