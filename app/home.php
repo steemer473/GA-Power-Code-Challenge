732d33b84ccba105ccf776728a26c111d6e95137
steemer473 <stephanie.ragsdale.2013@gmail.com>
2016-12-20 19:09:06 -0500

    3nd commit - added assets to git

---
+++ app/home.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html class="no-js">
+<head>
+    <?php include 'common/head.php';?>
+</head>
+<body>
+    <?php include 'common/menu.php';?>
+    <?php include 'common/header.php';?>
+    <div id="main" role="main">
+        <div class="container">
+            <!--Add Content Here...--->
+        </div>
+    </div>
+    <?php include 'common/footer.php';?>
+    <?php include 'common/foot.php';?>
+</body>
+</html>
